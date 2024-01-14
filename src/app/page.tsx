0303628cdb1d5b5394526ee69623ec2e83dc1ffd
Atai Barkai <atai.barkai@gmail.com>
2024-01-14 00:32:03 -0800

    add basic useMakeCopilotReadable

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
+"use client";
+
+import { useMakeCopilotReadable } from '@copilotkit/react-core';
 import Image from 'next/image'
 
 export default function Home() {
+
+  useMakeCopilotReadable('This is a nextJS template app');
+  useMakeCopilotReadable("The seecret number is 49");
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
